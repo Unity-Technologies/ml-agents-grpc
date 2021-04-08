95d0f18015dcaf0f322a4443c74a530dbe35bfb1
Christopher Goy <christopherg@unity3d.com>
2021-04-08 14:17:24 -0700

    Build a universal binary in one script.

--- unity/build_grpc_mac.sh
+++ unity/build_grpc_mac.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 # make sure an argument is passed
-if [ -z "$1" ]
-  then
-    echo "No argument supplied.  Expected 'arm64' or 'x86_64'"
-    exit 1
-fi
-
 set -o xtrace
 
+for arch in x86_64 arm64; do
+    # apply the compiler flag patch to boringssl
+    # assume we are in the repository root
+    pushd third_party/boringssl/
+    git apply ../../unity/bssl-01.patch
+    popd
+    
+    # start building with cmake
+    # https://grpc.io/docs/languages/cpp/quickstart/
+    mkdir -p cmake/build
+    pushd cmake/build
+    cmake -DgRPC_BUILD_TESTS=off -DCMAKE_OSX_ARCHITECTURES=$arch ../..
+    make -j
+    popd
+    
+    # copy the file we want into the artifacts folder
+    mkdir -p artifacts/grpc_$arch/
+    mv cmake/build/libgrpc_csharp_ext.dylib artifacts/grpc_$arch/libgrpc_csharp_ext.dylib
+done
 
-# apply the compiler flag patch to boringssl
-# assume we are in the repository root
-pushd third_party/boringssl/
-git apply ../../unity/bssl-01.patch
-popd
-
-# start building with cmake
-# https://grpc.io/docs/languages/cpp/quickstart/
-mkdir -p cmake/build
-pushd cmake/build
-cmake -DgRPC_BUILD_TESTS=off ../..
-make -j
-popd
-
-# copy the file we want into the artifacts folder
-mkdir -p artifacts/grpc_$1/
-mv cmake/build/libgrpc_csharp_ext.dylib artifacts/grpc_$1/libgrpc_csharp_ext.dylib
-
+mkdir -p artifacts/grpc_universal/
+lipo -create -output artifacts/grpc_universal/libgrpc_csharp_ext.dylib artifacts/grpc_x86_64/libgrpc_csharp_ext.dylib artifacts/grpc_arm64/libgrpc_csharp_ext.dylib
+codesign -s - -f artifacts/grpc_universal/libgrpc_csharp_ext.dylib
